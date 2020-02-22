c00cc759cce35c6aa297e8d79f65f918e2a93df8
Ken Diehl <diehlkj.99@gmail.com>
2020-02-21 21:25:48 -0500

    Created ProtectedRoute.js, axiosWithAuth.js, and useForm.js. Working on axiosWithAuth.js functionality.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React from 'react';
+import axiosWithAuth from '../utils/AxiosWithAuth';
+import useForm from '../utils/useForm';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const formSubmitCallback = () => {
+
+  };
+
+  const [values, handleChanges, handleSubmit] = useForm({
+    username: '',
+    password: ''
+  },
+    formSubmitCallback
+  );
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='username' value={values.username} onChange={handleChanges} />
+        <input type='password' name='password' value={values.password} onChange={handleChanges} />
+        <button type="submit">Sign In</button>
+      </form>
     </>
   );
 };
